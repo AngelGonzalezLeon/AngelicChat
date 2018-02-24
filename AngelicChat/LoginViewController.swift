a2a1e6fdae4e4b85bb5213b30575a8a9c43d8117
angel gonzalez <angelgonzalez@Mac.local>
2018-02-23 18:15:19 -0800

    login and signin work

--- AngelicChat/LoginViewController.swift
+++ AngelicChat/LoginViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,39 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func onSignIn(_ sender: Any) {
+        PFUser.logInWithUsername(inBackground: usernameField.text!, password: passwordField.text!){(
+            user, error) -> Void in
+            if user != nil {
+                print("logged in")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+    }
+    @IBAction func onSignUp(_ sender: Any) {
+        let newUser = PFUser()
+        
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        newUser.signUpInBackground() { (success, error) in
+            if success
+            {
+                print("Created a badass user")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else
+            {
+                let e = error! as NSError
+                print(e.localizedDescription as Any)
+                
+                if e.code == 202 {
+                    print("User name is taken")
+                }
+            }
+        }
+    }
+    
 
 }
 
